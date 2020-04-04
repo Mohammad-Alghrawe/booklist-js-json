f5fcb4effdb40e2b7f45e0dc87a49b18b0112376
Mohammad Alghrawe <mohammadalghrawe@gmail.com>
2020-04-04 13:09:23 +0200

    created an event listener for our input form

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   //What this is doing is taking the entire document , which is the whole HTML page we are working on, and listens for the event of 'DOMContentLoaded' which is basically when the entire DOM has finished loading in. Once that event is heard, it then runs whatever code we put inside the curly brackets
   const bookContainer = document.querySelector("#book-container");
-const bookURL = `http://localhost:3000/books`;
+  const bookURL = `http://localhost:3000/books`;
 
-fetch(`${bookURL}`)
-    .then( response => response.json() )
-    .then( bookData => bookData.forEach(function(book) {
-      bookContainer.innerHTML += `
+  fetch(`${bookURL}`)
+    .then(response => response.json())
+    .then(bookData =>
+      bookData.forEach(function(book) {
+        bookContainer.innerHTML += `
       <div id=${book.id}>
         <h2>${book.title}</h2>
         <h4>Author: ${book.author}</h4>
@@ -14,9 +15,14 @@ fetch(`${bookURL}`)
         <p>${book.description}</p>
         <button data-id="${book.id}" id="edit-${book.id}" data-action="edit">Edit</button>
         <button data-id="${book.id}" id="delete-${book.id}" data-action="delete">Delete</button>
-      </div>`
-    })) // end of book fetch
+      </div>`;
+      })
+    ); // end of book fetch
 
-    const bookForm = document.querySelector("#book-form");
-
+  const bookForm = document.querySelector("#book-form");
+  bookForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log(e.target);
+    
+  });
 });
