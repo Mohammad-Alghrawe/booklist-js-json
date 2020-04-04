a38243bbb42ec75cb8f39190fd72c65a3f1f8fbd
Mohammad Alghrawe <mohammadalghrawe@gmail.com>
2020-04-04 13:39:03 +0200

    added a methods to render the data without refreshing the page

--- script.js
+++ script.js
@@ -39,6 +39,18 @@ document.addEventListener("DOMContentLoaded", function() {
       headers: {
         "Content-Type": "application/json"
       }
-    });
+    })
+      .then(response => response.json())
+      .then(book => {
+        bookContainer.innerHTML += `
+        <div id=${book.id}>
+          <h2>${book.title}</h2>
+          <h4>Author: ${book.author}</h4>
+          <img src="${book.coverImage}" width="333" height="500">
+          <p>${book.description}</p>
+          <button data-id="${book.id}" id="edit-${book.id}" data-action="edit">Edit</button>
+          <button data-id="${book.id}" id="delete-${book.id}" data-action="delete">Delete</button>
+        </div>`;
+      });
   });
 });
