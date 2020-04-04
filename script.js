dd6e8105b63f839425273224cb9f6eb35ee86779
Mohammad Alghrawe <mohammadalghrawe@gmail.com>
2020-04-04 12:54:46 +0200

    created a constant for the URL of our books database

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   //What this is doing is taking the entire document , which is the whole HTML page we are working on, and listens for the event of 'DOMContentLoaded' which is basically when the entire DOM has finished loading in. Once that event is heard, it then runs whatever code we put inside the curly brackets
   const bookContainer = document.querySelector("#book-container");
-
+const bookURL = `http://localhost:3000/books`;
 });
